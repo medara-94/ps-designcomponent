c53ca81a1bf8b37b98f17ffd86441ad31a0da5e8
Alex <bertoncin@bludata.com>
2022-04-22 14:31:50 +0200

    4.2 Async promise su useEffect

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7e960065a5604618"; }
+/******/ 		__webpack_require__.h = function() { return "00081fe807f7f489"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/SpeakersList.js
+++ src/components/SpeakersList.js
@@ -1,10 +1,21 @@
 import Speaker from './Speaker';
 import {data} from "../../SpeakerData";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 function SpeakersList({ showSession}) {
 
-    const [speakersData, setSpeakersData] = useState(data);
+    const [speakersData, setSpeakersData] = useState([]); 
+
+    const delay = (ms) => new Promise((resolve) => setTimeout (resolve,ms));
+
+    // Lo useEffect non è asincrono, quindi mi darà errore la promise, quindi creo una funzione async dentro
+    useEffect(() => {
+        async function delayFunc(){
+            await delay(2000);
+            setSpeakersData(data);
+        }
+        delayFunc();           
+    });
 
     function onFavoriteToggle(id) {
         //Recupero la referenza dell'oggetto che voglio modificare

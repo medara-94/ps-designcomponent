d6a0e2384bf28b0d817f44ab3d860c13abda9cc6
Alex <bertoncin@bludata.com>
2022-05-02 14:13:39 +0200

    5.4 Separate State Management from our Theme Context with a Custom Hook

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7641791c3628f9c2"; }
+/******/ 		__webpack_require__.h = function() { return "f7cb006a3b86c195"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/Contexts/ThemeContext.js
+++ src/Contexts/ThemeContext.js
@@ -1,10 +1,12 @@
-import { useState, createContext } from "react";
+import { createContext } from "react";
+import useTheme from "../hooks/useTheme";
 
 export const ThemeContext = createContext();
 
 
 function ThemeProvider({startingTheme, children}) {
-    const [theme, setTheme] = useState(startingTheme);
+    //Uso le {} e non le [] come negli useState perchè il return del componente useTheme è un oggetto
+    const {theme, setTheme} = useTheme(startingTheme);
 
     return (
         //{Children} renderizza tutti i componenti figli, quindi nel nostro caso Header e Speakers
